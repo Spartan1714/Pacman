a28ae0325bdedcae1baab236b46801fb042e1e23
Spatan1714 <javierriveraaguilera04@gmail.com>
2026-03-06 14:16:56 -0600

    firebase

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDDjYUCgsPmifR0hNTaw3aD9Qg5dyjDdxM",
+  authDomain: "pacman-game-e602a.firebaseapp.com",
+  projectId: "pacman-game-e602a",
+  storageBucket: "pacman-game-e602a.firebasestorage.app",
+  messagingSenderId: "316960307396",
+  appId: "1:316960307396:web:2b33cecfd8b3dde621f191",
+  measurementId: "G-ER40M0DJ2M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
