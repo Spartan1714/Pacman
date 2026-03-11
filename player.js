8caabe229b99290490edfaedcaf02a91fec68038
Spatan1714 <javierriveraaguilera04@gmail.com>
2026-03-11 13:17:39 -0600

    pixeles

--- player.js
+++ player.js
@@ -1,58 +1,78 @@
 import { map, TILE_SIZE } from "./map.js";
 
 export let pacman = {
-x: TILE_SIZE*1.5,
-y: TILE_SIZE*1.5,
-dx:0,
-dy:0,
-speed:100
+    x: TILE_SIZE * 1.5,
+    y: TILE_SIZE * 1.5,
+    vx: 0,
+    vy: 0,
+    speed: 120
 };
 
-export function setDirection(dx,dy){
-pacman.dx = dx;
-pacman.dy = dy;
+export function setDirection(dx, dy){
+
+    pacman.vx = dx * pacman.speed;
+    pacman.vy = dy * pacman.speed;
+
 }
 
 export function updatePlayer(scoreRef, deltaTime){
 
-let move = pacman.speed * deltaTime / 1000;
+    let nextX = pacman.x + pacman.vx * deltaTime / 1000;
+    let nextY = pacman.y + pacman.vy * deltaTime / 1000;
 
-let nextX = pacman.x + pacman.dx * move;
-let nextY = pacman.y + pacman.dy * move;
+    let tileX = Math.floor(nextX / TILE_SIZE);
+    let tileY = Math.floor(nextY / TILE_SIZE);
 
-let tileX = Math.floor(nextX / TILE_SIZE);
-let tileY = Math.floor(nextY / TILE_SIZE);
+    if(map[tileY] && map[tileY][tileX] !== 1){
 
-if(map[tileY] && map[tileY][tileX] !== 1){
+        pacman.x = nextX;
+        pacman.y = nextY;
 
-pacman.x = nextX;
-pacman.y = nextY;
+        if(map[tileY][tileX] === 2){
 
-if(map[tileY][tileX] === 2){
-map[tileY][tileX] = 0;
-scoreRef.value += 10;
-}
+            map[tileY][tileX] = 0;
+            scoreRef.value += 10;
 
-}
+        }
+
+    }
 
 }
 
 export function drawPlayer(ctx){
 
-ctx.fillStyle="yellow";
+    let startAngle = 0.2 * Math.PI;
+    let endAngle = 1.8 * Math.PI;
 
-ctx.beginPath();
+    if(pacman.vx < 0){
+        startAngle = 1.2 * Math.PI;
+        endAngle = 0.8 * Math.PI;
+    }
 
-ctx.arc(
-pacman.x,
-pacman.y,
-TILE_SIZE/2-2,
-0.2*Math.PI,
-1.8*Math.PI
-);
+    if(pacman.vy < 0){
+        startAngle = 1.7 * Math.PI;
+        endAngle = 1.3 * Math.PI;
+    }
 
-ctx.lineTo(pacman.x,pacman.y);
+    if(pacman.vy > 0){
+        startAngle = 0.7 * Math.PI;
+        endAngle = 0.3 * Math.PI;
+    }
 
-ctx.fill();
+    ctx.fillStyle = "yellow";
+
+    ctx.beginPath();
+
+    ctx.arc(
+        pacman.x,
+        pacman.y,
+        TILE_SIZE/2 - 2,
+        startAngle,
+        endAngle
+    );
+
+    ctx.lineTo(pacman.x, pacman.y);
+
+    ctx.fill();
 
 }
