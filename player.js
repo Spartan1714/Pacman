62b7dd02edd5e774a985f69b994bf7cd7fb63b09
Spatan1714 <javierriveraaguilera04@gmail.com>
2026-03-11 12:39:54 -0600

    prueba

---
+++ player.js
@@ -0,0 +1,57 @@
+import { map, TILE_SIZE } from "./map.js";
+
+export let pacman = {
+    x: 1,
+    y: 1,
+    dx: 0,
+    dy: 0
+};
+
+export function setDirection(dx, dy){
+    pacman.dx = dx;
+    pacman.dy = dy;
+}
+
+export function updatePlayer(scoreRef){
+
+    let nextX = pacman.x + pacman.dx;
+    let nextY = pacman.y + pacman.dy;
+
+    if(map[nextY] && map[nextY][nextX] !== 1){
+
+        pacman.x = nextX;
+        pacman.y = nextY;
+
+        if(map[nextY][nextX] === 2){
+
+            map[nextY][nextX] = 0;
+            scoreRef.value += 10;
+
+        }
+
+    }
+
+}
+
+export function drawPlayer(ctx){
+
+    ctx.fillStyle = "yellow";
+
+    ctx.beginPath();
+
+    ctx.arc(
+        pacman.x*TILE_SIZE + TILE_SIZE/2,
+        pacman.y*TILE_SIZE + TILE_SIZE/2,
+        TILE_SIZE/2-2,
+        0.2*Math.PI,
+        1.8*Math.PI
+    );
+
+    ctx.lineTo(
+        pacman.x*TILE_SIZE + TILE_SIZE/2,
+        pacman.y*TILE_SIZE + TILE_SIZE/2
+    );
+
+    ctx.fill();
+
+}
