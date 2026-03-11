4e5ad27768d76309ade256740f143eb5e1919465
Spatan1714 <javierriveraaguilera04@gmail.com>
2026-03-11 14:31:02 -0600

    player y game cambiados

--- player.js
+++ player.js
@@ -1,91 +1,86 @@
-import { map, TILE_SIZE } from "./map.js";
+import { map } from "./map.js";
 
 export let pacman = {
-    x: 1,
-    y: 1,
-    dx: 0,
-    dy: 0
+x:1,
+y:1,
+dx:0,
+dy:0
 };
 
 let lastMoveTime = 0;
 const MOVE_DELAY = 120;
-export function setDirection(dx, dy){
-    pacman.dx = dx;
-    pacman.dy = dy;
+
+export function setDirection(dx,dy){
+
+pacman.dx = dx;
+pacman.dy = dy;
+
 }
 
 export function updatePlayer(scoreRef){
 
-    let now = Date.now();
+let now = Date.now();
 
-    if(now - lastMoveTime < MOVE_DELAY) return;
+if(now - lastMoveTime < MOVE_DELAY) return;
 
-    let nextX = pacman.x + pacman.dx;
-    let nextY = pacman.y + pacman.dy;
+let nextX = pacman.x + pacman.dx;
+let nextY = pacman.y + pacman.dy;
 
-    if(map[nextY] && map[nextY][nextX] !== 1){
+if(map[nextY] && map[nextY][nextX] !== 1){
 
-        pacman.x = nextX;
-        pacman.y = nextY;
+pacman.x = nextX;
+pacman.y = nextY;
 
-        if(map[nextY][nextX] === 2){
+if(map[nextY][nextX] === 2){
 
-            map[nextY][nextX] = 0;
-            scoreRef.value += 10;
-
-        }
-
-    }
-
-    lastMoveTime = now;
+map[nextY][nextX] = 0;
+scoreRef.value += 10;
 
 }
 
-export function drawPlayer(ctx){
+}
 
-    ctx.fillStyle = "yellow";
+lastMoveTime = now;
 
-    let startAngle = 0.2 * Math.PI;
-    let endAngle = 1.8 * Math.PI;
+}
 
-    // derecha
-    if(pacman.dx === 1){
-        startAngle = 0.2 * Math.PI;
-        endAngle = 1.8 * Math.PI;
-    }
+export function drawPlayer(ctx,tileSize,offsetX,offsetY){
 
-    // izquierda
-    if(pacman.dx === -1){
-        startAngle = 1.2 * Math.PI;
-        endAngle = 0.8 * Math.PI;
-    }
+ctx.fillStyle="yellow";
 
-    // arriba
-    if(pacman.dy === -1){
-        startAngle = 1.7 * Math.PI;
-        endAngle = 1.3 * Math.PI;
-    }
+let startAngle = 0.2*Math.PI;
+let endAngle = 1.8*Math.PI;
 
-    // abajo
-    if(pacman.dy === 1){
-        startAngle = 0.7 * Math.PI;
-        endAngle = 0.3 * Math.PI;
-    }
+if(pacman.dx === -1){
+startAngle = 1.2*Math.PI;
+endAngle = 0.8*Math.PI;
+}
 
-    ctx.beginPath();
+if(pacman.dy === -1){
+startAngle = 1.7*Math.PI;
+endAngle = 1.3*Math.PI;
+}
 
-    ctx.arc(
-        offsetX + pacman.x,
-        offsetY + pacman.y,
-        TILE_SIZE/2 - 2,
-        startAngle,
-        endAngle
-    );
+if(pacman.dy === 1){
+startAngle = 0.7*Math.PI;
+endAngle = 0.3*Math.PI;
+}
 
-    ctx.lineTo(
-        pacman.x * TILE_SIZE + TILE_SIZE/2,
-        pacman.y * TILE_SIZE + TILE_SIZE/2
-    );
+ctx.beginPath();
 
-    ctx.fill();
+ctx.arc(
+offsetX + pacman.x*tileSize + tileSize/2,
+offsetY + pacman.y*tileSize + tileSize/2,
+tileSize/2 - 2,
+startAngle,
+endAngle
+);
+
+ctx.lineTo(
+offsetX + pacman.x*tileSize + tileSize/2,
+offsetY + pacman.y*tileSize + tileSize/2
+);
+
+ctx.fill();
+
 }
