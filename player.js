85ad80258ee62b1e473b366720de42a1679bf870
Spatan1714 <javierriveraaguilera04@gmail.com>
2026-03-11 13:04:27 -0600

    rotacion Pacman

--- player.js
+++ player.js
@@ -45,21 +45,47 @@ export function drawPlayer(ctx){
 
     ctx.fillStyle = "yellow";
 
+    let startAngle = 0.2 * Math.PI;
+    let endAngle = 1.8 * Math.PI;
+
+    // derecha
+    if(pacman.dx === 1){
+        startAngle = 0.2 * Math.PI;
+        endAngle = 1.8 * Math.PI;
+    }
+
+    // izquierda
+    if(pacman.dx === -1){
+        startAngle = 1.2 * Math.PI;
+        endAngle = 0.8 * Math.PI;
+    }
+
+    // arriba
+    if(pacman.dy === -1){
+        startAngle = 1.7 * Math.PI;
+        endAngle = 1.3 * Math.PI;
+    }
+
+    // abajo
+    if(pacman.dy === 1){
+        startAngle = 0.7 * Math.PI;
+        endAngle = 0.3 * Math.PI;
+    }
+
     ctx.beginPath();
 
     ctx.arc(
-        pacman.x*TILE_SIZE + TILE_SIZE/2,
-        pacman.y*TILE_SIZE + TILE_SIZE/2,
-        TILE_SIZE/2-2,
-        0.2*Math.PI,
-        1.8*Math.PI
+        pacman.x * TILE_SIZE + TILE_SIZE/2,
+        pacman.y * TILE_SIZE + TILE_SIZE/2,
+        TILE_SIZE/2 - 2,
+        startAngle,
+        endAngle
     );
 
     ctx.lineTo(
-        pacman.x*TILE_SIZE + TILE_SIZE/2,
-        pacman.y*TILE_SIZE + TILE_SIZE/2
+        pacman.x * TILE_SIZE + TILE_SIZE/2,
+        pacman.y * TILE_SIZE + TILE_SIZE/2
     );
 
     ctx.fill();
-
 }
