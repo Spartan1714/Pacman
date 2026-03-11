602d80a6f04f73ec4de5c7e113e00e9d52f03de2
Spatan1714 <javierriveraaguilera04@gmail.com>
2026-03-11 12:51:01 -0600

    game.js

--- game.js
+++ game.js
@@ -1,37 +1,16 @@
-import { map, TILE_SIZE } from "map.js";
-import { updatePlayer, drawPlayer, setDirection } from "player.js";
+import { map, TILE_SIZE } from "./map.js";
+import { updatePlayer, drawPlayer, setDirection } from "./player.js";
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-const tileSize = 24;
-
-let score = 0;
-
-const map = [
-
-[1,1,1,1,1,1,1,1,1,1],
-[1,2,2,2,2,2,2,2,2,1],
-[1,2,1,1,1,2,1,1,2,1],
-[1,2,2,2,1,2,2,2,2,1],
-[1,1,1,2,1,1,1,2,1,1],
-[1,2,2,2,2,2,2,2,2,1],
-[1,1,1,1,1,1,1,1,1,1]
-
-];
-
-
-let pacman = {
-x:1,
-y:1,
-dx:0,
-dy:0
-};
+let score = { value: 0 };
 
 function drawMap(){
 
-for(let y=0; y<map.length; y++){
+for(let y = 0; y < map.length; y++){
 
-for(let x=0; x<map[y].length; x++){
+for(let x = 0; x < map[y].length; x++){
 
 let tile = map[y][x];
 
@@ -52,7 +31,6 @@ if(tile === 2){
 ctx.fillStyle = "white";
 
 ctx.beginPath();
-
 ctx.arc(
 x*TILE_SIZE + TILE_SIZE/2,
 y*TILE_SIZE + TILE_SIZE/2,
@@ -69,102 +47,19 @@ ctx.fill();
 
 }
 
-} 
-
-function drawMap(){
-
-for(let y=0;y<map.length;y++){
-
-for(let x=0;x<map[y].length;x++){
-
-let tile = map[y][x];
-
-if(tile === 1){
-
-ctx.fillStyle = "blue";
-ctx.fillRect(x*tileSize,y*tileSize,tileSize,tileSize);
-
-}
-
-if(tile === 2){
-
-ctx.fillStyle = "white";
-
-ctx.beginPath();
-ctx.arc(
-x*tileSize+tileSize/2,
-y*tileSize+tileSize/2,
-3,
-0,
-Math.PI*2
-);
-
-ctx.fill();
-
-}
-
-}
-
-}
-
-}
-
-function drawPacman(){
-
-ctx.fillStyle = "yellow";
-
-ctx.beginPath();
-
-ctx.arc(
-pacman.x*tileSize + tileSize/2,
-pacman.y*tileSize + tileSize/2,
-tileSize/2-2,
-0.2*Math.PI,
-1.8*Math.PI
-);
-
-ctx.lineTo(
-pacman.x*tileSize + tileSize/2,
-pacman.y*tileSize + tileSize/2
-);
-
-ctx.fill();
-
-}
-
-function movePacman(){
-
-let nextX = pacman.x + pacman.dx;
-let nextY = pacman.y + pacman.dy;
-
-if(map[nextY][nextX] !== 1){
-
-pacman.x = nextX;
-pacman.y = nextY;
-
-if(map[nextY][nextX] === 2){
-
-map[nextY][nextX] = 0;
-score += 10;
-
-}
-
-}
-
 }
 
 function drawScore(){
 
 ctx.fillStyle = "white";
 ctx.font = "16px Arial";
-
-ctx.fillText("Score: "+score,10,20);
+ctx.fillText("Score: " + score.value, 10, 20);
 
 }
 
 function update(){
 
-movePacman();
+updatePlayer(score);
 
 }
 
@@ -173,7 +68,7 @@ function draw(){
 ctx.clearRect(0,0,canvas.width,canvas.height);
 
 drawMap();
-drawPacman();
+drawPlayer(ctx);
 drawScore();
 
 }
@@ -187,27 +82,12 @@ requestAnimationFrame(gameLoop);
 
 }
 
-document.addEventListener("keydown",e=>{
+document.addEventListener("keydown", e => {
 
-if(e.key === "ArrowUp"){
-pacman.dx=0;
-pacman.dy=-1;
-}
-
-if(e.key === "ArrowDown"){
-pacman.dx=0;
-pacman.dy=1;
-}
-
-if(e.key === "ArrowLeft"){
-pacman.dx=-1;
-pacman.dy=0;
-}
-
-if(e.key === "ArrowRight"){
-pacman.dx=1;
-pacman.dy=0;
-}
+if(e.key === "ArrowUp") setDirection(0,-1);
+if(e.key === "ArrowDown") setDirection(0,1);
+if(e.key === "ArrowLeft") setDirection(-1,0);
+if(e.key === "ArrowRight") setDirection(1,0);
 
 });
 
