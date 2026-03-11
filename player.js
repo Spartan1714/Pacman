a3c07942e80c82b8e059aed5a897185e82f4fa2b
Spatan1714 <javierriveraaguilera04@gmail.com>
2026-03-11 13:44:03 -0600

    si

--- player.js
+++ player.js
@@ -7,6 +7,8 @@ export let pacman = {
     dy: 0
 };
 
+let lastMoveTime = 0;
+const MOVE_DELAY = 120;
 export function setDirection(dx, dy){
     pacman.dx = dx;
     pacman.dy = dy;
@@ -14,35 +16,28 @@ export function setDirection(dx, dy){
 
 export function updatePlayer(scoreRef){
 
- let lastMoveTime = 0;
-const MOVE_DELAY = 120;
+    let now = Date.now();
 
-export function updatePlayer(scoreRef){
+    if(now - lastMoveTime < MOVE_DELAY) return;
 
-let now = Date.now();
+    let nextX = pacman.x + pacman.dx;
+    let nextY = pacman.y + pacman.dy;
 
-if(now - lastMoveTime < MOVE_DELAY) return;
+    if(map[nextY] && map[nextY][nextX] !== 1){
 
-let nextX = pacman.x + pacman.dx;
-let nextY = pacman.y + pacman.dy;
+        pacman.x = nextX;
+        pacman.y = nextY;
 
-if(map[nextY] && map[nextY][nextX] !== 1){
+        if(map[nextY][nextX] === 2){
 
-pacman.x = nextX;
-pacman.y = nextY;
+            map[nextY][nextX] = 0;
+            scoreRef.value += 10;
 
-if(map[nextY][nextX] === 2){
+        }
 
-map[nextY][nextX] = 0;
-scoreRef.value += 10;
+    }
 
-}
-
-}
-
-lastMoveTime = now;
-
-}
+    lastMoveTime = now;
 
 }
 
@@ -50,21 +45,47 @@ export function drawPlayer(ctx){
 
     ctx.fillStyle = "yellow";
 
+    let startAngle = 0.2 * Math.PI;
+    let endAngle = 1.8 * Math.PI;
+
+    // derecha
+    if(pacman.dx === 1){
+        startAngle = 0.2 * Math.PI;
+        endAngle = 1.8 * Math.PI;
+    }
+
+    // izquierda
+    if(pacman.dx === -1){
+        startAngle = 1.2 * Math.PI;
+        endAngle = 0.8 * Math.PI;
+    }
+
+    // arriba
+    if(pacman.dy === -1){
+        startAngle = 1.7 * Math.PI;
+        endAngle = 1.3 * Math.PI;
+    }
+
+    // abajo
+    if(pacman.dy === 1){
+        startAngle = 0.7 * Math.PI;
+        endAngle = 0.3 * Math.PI;
+    }
+
     ctx.beginPath();
 
     ctx.arc(
-        pacman.x*TILE_SIZE + TILE_SIZE/2,
-        pacman.y*TILE_SIZE + TILE_SIZE/2,
-        TILE_SIZE/2-2,
-        0.2*Math.PI,
-        1.8*Math.PI
+        pacman.x * TILE_SIZE + TILE_SIZE/2,
+        pacman.y * TILE_SIZE + TILE_SIZE/2,
+        TILE_SIZE/2 - 2,
+        startAngle,
+        endAngle
     );
 
     ctx.lineTo(
-        pacman.x*TILE_SIZE + TILE_SIZE/2,
-        pacman.y*TILE_SIZE + TILE_SIZE/2
+        pacman.x * TILE_SIZE + TILE_SIZE/2,
+        pacman.y * TILE_SIZE + TILE_SIZE/2
     );
 
     ctx.fill();
-
 }
