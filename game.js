ec744b85e360b6558f60d25e039ec2c610aa6911
Spatan1714 <javierriveraaguilera04@gmail.com>
2026-03-11 11:41:55 -0600

    style y game

--- game.js
+++ game.js
@@ -1,9 +1,38 @@
-import { db, ref, push } from "./firebase.js";
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
-console.log("Guardando score");
+let pacman = {
+    x: 50,
+    y: 50,
+    size: 20,
+    speed: 5
+};
 
-push(ref(db, "scores"), {
-  player: "Osejo  ",
-  score: 100,
-  date: Date.now()
+function drawPacman() {
+    ctx.beginPath();
+    ctx.arc(pacman.x, pacman.y, pacman.size, 0.2 * Math.PI, 1.8 * Math.PI);
+    ctx.lineTo(pacman.x, pacman.y);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+}
+
+function update(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawPacman();
+}
+
+document.addEventListener("keydown", (e)=>{
+
+    if(e.key === "ArrowUp") pacman.y -= pacman.speed;
+    if(e.key === "ArrowDown") pacman.y += pacman.speed;
+    if(e.key === "ArrowLeft") pacman.x -= pacman.speed;
+    if(e.key === "ArrowRight") pacman.x += pacman.speed;
+
 });
+
+function gameLoop(){
+    update();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
